7ca18cc4a555f05249e303d023e55d32a39a72a0
Kelvin Luck <kelvin.luck@crowdstrike.com>
2018-02-08 13:31:40 -0800

    Make the demo work sensibly with a fixed number of concurrent tasks
    Priority is still ignored but the tasks do fgo through the flow correctly

--- addon/services/priority-queue.js
+++ addon/services/priority-queue.js
@@ -1,3 +1,4 @@
+import EmberObject from '@ember/object';
 import Service from '@ember/service';
 import { A } from '@ember/array';
 import { computed } from '@ember/object';
@@ -9,14 +10,15 @@ export default Service.extend({
   queuedTasks: computed.readOnly('_queuedTasks'),
 
   addItem({ priority, task }) {
-    this.get('_allTasks').addObject({
+    this.get('_allTasks').addObject(EmberObject.create({
       _addedAt: Date.now(),
       _isComplete: false,
       _isRunning: false,
       _isQueued: true,
       priority,
       task,
-    });
+    }));
+    this._fillQueue();
   },
 
   clear() {
@@ -32,13 +34,45 @@ export default Service.extend({
   start() {
     if (!this.get('_isRunning')) {
       this.set('_isRunning', true);
+      this._fillQueue();
     }
   },
 
   _allTasks: computed(() =>  A([])),
   _completeTasks: computed.filterBy('_allTasks', '_isComplete', true),
   _isRunning: false,
+  _maxConcurrentTasks: 3,
   _runningTasks: computed.filterBy('_allTasks', '_isRunning', true),
   _queuedTasks: computed.filterBy('_allTasks', '_isQueued', true),
+
+  _fillQueue() {
+    if (!this.get('_isRunning')) {
+      return;
+    }
+
+    let maxConcurrentTasks = this.get('_maxConcurrentTasks');
+    while(this.get('_runningTasks.length') < maxConcurrentTasks) {
+      let nextTask = this.get('_queuedTasks.firstObject');
+      if (!nextTask) {
+        break; // queue is empty
+      }
+      this._runTask(nextTask);
+    }
+  },
+
+  _runTask(task) {
+    task.setProperties({
+      _isRunning: true,
+      _isQueued: false,
+    });
+    task.get('task')()
+      .then(() => {
+        task.setProperties({
+          _isComplete: true,
+          _isRunning: false,
+        })
+        this._fillQueue();
+      });
+  },
 
 });
